3239bd426ea0e4cfc6889d88f801777f644a2a39
Michael Karamanov <michaelkaramanov@Michaels-MacBook-Air-3.local>
2021-02-18 12:56:40 -0500

    Initial pass at index view (home page) and index.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from recipes.models import Recipe, Ingredient, Instruction
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Fetch the number of recipe records
+    num_recipes = Recipe.objects.count()
+    newest_recipes = Recipe.objects.order_by('-pk')[:10]
+
+    context ={
+        'num_recipes': num_recipes,
+        'newest_recipes': newest_recipes
+    }
+
+    return render(request, 'index.html', context=context)
+
