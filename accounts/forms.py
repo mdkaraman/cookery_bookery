1861120bc34cde789016b3fb0ff597c6f28f963a
Michael Karamanov <michaelkaramanov@Michaels-MacBook-Air-3.local>
2021-02-19 11:22:23 -0500

    account/forms.py created and email field added to signup page; signup.html customized for error messages

---
+++ accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=200)
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError(_("This email address belongs to an existing account! Enter a new email."))
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
