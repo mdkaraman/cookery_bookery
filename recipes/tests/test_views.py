12c1f36cd7c685a1ff2c12e64e95fb25296f131c
Michael Karamanov <michaelkaramanov@Michaels-MacBook-Air-3.local>
2021-02-24 14:37:27 -0500

    Added test coverage for RecipeCreate view

--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 from recipes.models import Recipe, Ingredient, Instruction
+
+from recipes.views import CustomCreateMixin
+from recipes.views import CustomUpdateOrDeleteMixin
+from recipes.views import CustomUpdateMixin, CustomDeleteMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 class IndexViewTest(TestCase):
     @classmethod
@@ -97,3 +104,40 @@ class RecipeDetailViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'recipes/recipe_detail.html')
 
+
+class RecipeCreateViewTest(TestCase):
+    
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user1.save()
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('create-recipe'))
+        self.assertRedirects(response, '/accounts/login/?next=/recipes/submit-recipe/create')
+
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('create-recipe'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipes/recipe_form.html')
+'''
+class CustomCreateMixinTest(TestCase):
+    
+    class DummyRecipeCreateView(CustomCreateMixin, CreateView):
+        model = Recipe
+        #fields = ['name', 'servings', 'nota_bene']
+
+
+
+    def setUp(cls):
+        # Create test users
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+
+        test_user1.save()
+        test_user2.save()
+
+        # Create recipe
+        recipe = Recipe.objects.create(name='Pasta', servings='4', nota_bene='')
+'''
