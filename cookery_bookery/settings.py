1ac0b76e2cbe63e718f09b8cb777e2a39a85a7e8
Michael Karamanov <michaelkaramanov@Michaels-MacBook-Air-3.local>
2021-03-13 13:26:36 -0500

    Fixed build path bug in settings

--- cookery_bookery/settings.py
+++ cookery_bookery/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -139,7 +138,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles' #. os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'  
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
