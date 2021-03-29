16836a0bf8afd0fa987a81ec1640f9fd129a8036
Michael Karamanov <michaelkaramanov@Michaels-MacBook-Air-3.local>
2021-03-29 14:10:20 -0400

    Improved url patterns

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,33 +5,33 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("all-recipes/", views.RecipeListView.as_view(), name="all-recipes"),
-    path("recipe/<int:pk>", views.RecipeDetailView.as_view(), name="recipe-detail"),
+    path("all/", views.RecipeListView.as_view(), name="all-recipes"),
+    path("<int:pk>", views.RecipeDetailView.as_view(), name="recipe-detail"),
     path("my-recipes/", views.MyRecipesListView.as_view(), name="my-recipes"),
     path("my-favorites/", views.MyFavoritesListView.as_view(), name="my-favorites"),
-    path("submit-recipe/create/", views.RecipeCreate.as_view(), name="create-recipe"),
+    path("submit/create/", views.RecipeCreate.as_view(), name="create-recipe"),
     path(
-        "submit-recipe/<int:pk>/add-ingredient",
+        "submit/<int:pk>/add-ingredient",
         views.IngredientCreate.as_view(),
         name="add-ingredient",
     ),
     path(
-        "submit-recipe/<int:pk>/add-instruction",
+        "submit/<int:pk>/add-instruction",
         views.InstructionCreate.as_view(),
         name="add-instruction",
     ),
     path(
-        "submit-recipe/<int:pk>/update_recipe",
+        "submit/<int:pk>/update_recipe",
         views.RecipeUpdate.as_view(),
         name="update-recipe",
     ),
     path(
-        "submit-recipe/<int:pk>/update_ingredient",
+        "submit/<int:pk>/update_ingredient",
         views.IngredientUpdate.as_view(),
         name="update-ingredient",
     ),
     path(
-        "submit-recipe/<int:pk>/update_instruction",
+        "submit/<int:pk>/update_instruction",
         views.InstructionUpdate.as_view(),
         name="update-instruction",
     ),
@@ -39,12 +39,12 @@ urlpatterns = [
         "recipe/<int:pk>/delete", views.RecipeDeleteView.as_view(), name="delete-recipe"
     ),
     path(
-        "submit-recipe/<int:pk>/delete_ingredient",
+        "submit/<int:pk>/delete_ingredient",
         views.IngredientDelete.as_view(),
         name="delete-ingredient",
     ),
     path(
-        "submit-recipe/<int:pk>/delete_instruction",
+        "submit/<int:pk>/delete_instruction",
         views.InstructionDelete.as_view(),
         name="delete-instruction",
     ),
